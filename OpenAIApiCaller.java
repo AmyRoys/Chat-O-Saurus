20de2ed07e252e96e434f29af0059808a99f60da
dominickstephens <dominickdstephens@gmail.com>
2023-05-16 23:10:18 +0100

    formatting

--- OpenAIApiCaller.java
+++ OpenAIApiCaller.java
@@ -4,7 +4,6 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.concurrent.CompletableFuture;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
--- PipeExample.java
+++ PipeExample.java
@@ -3,7 +3,6 @@ import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.net.URISyntaxException;
 import java.util.Scanner;
-import java.util.concurrent.CompletableFuture;
 
 public class PipeExample {
 
@@ -14,48 +13,40 @@ public class PipeExample {
         final PipedOutputStream output2 = new PipedOutputStream();
         final PipedInputStream input2 = new PipedInputStream(output2);
 
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int data = input1.read();
-                    StringBuilder modifiedData = new StringBuilder();
+        Thread thread1 = new Thread(() -> {
+            try {
+                int data = input1.read();
+                StringBuilder modifiedData = new StringBuilder();
 
-                    while (data != -1) {
-                        char character = (char) data;
-                        modifiedData.append(character);
-                        data = input1.read();
-                    }
-                    String response = OpenAIApiCaller.callOpenAIApi(modifiedData.toString());
-                    output2.write(response.toString().getBytes());
-                    output2.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (URISyntaxException e) {
-                    throw new RuntimeException(e);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                while (data != -1) {
+                    char character = (char) data;
+                    modifiedData.append(character);
+                    data = input1.read();
                 }
+                String response = OpenAIApiCaller.callOpenAIApi(modifiedData.toString());
+                output2.write(response.getBytes());
+                output2.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException | InterruptedException e) {
+                throw new RuntimeException(e);
             }
         });
 
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Scanner sc= new Scanner(System.in);
-                    System.out.print("You are in Thread 2 - Enter your prompt: ");
-                    String prompt = sc.nextLine();
-                    output1.write(prompt.getBytes());
-                    output1.close();
-                    int data = input2.read();
-                    while (data != -1) {
-                        System.out.print((char) data);
-                        data = input2.read();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+        Thread thread2 = new Thread(() -> {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("You are in Thread 2 - Enter your prompt: ");
+                String prompt = sc.nextLine();
+                output1.write(prompt.getBytes());
+                output1.close();
+                int data = input2.read();
+                while (data != -1) {
+                    System.out.print((char) data);
+                    data = input2.read();
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
 
