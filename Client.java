c0f658b55279748c7137a3311e6d54a6d220fb63
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2023-05-18 20:18:27 +0100

    Add functionality to Client

--- Client.java
+++ Client.java
@@ -1,6 +1,6 @@
-import java.net.*;
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
         private Socket socket;
@@ -15,7 +15,7 @@ public class Client {
                 this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                 this.clientUsername = clientUsername;
             }catch(Exception e){
-                closeEverything(socket, bufferedWriter, bufferedReader);
+                closeEverything(socket, bufferedReader, bufferedWriter);
             }
         }
         public void sendMessage(){
@@ -27,16 +27,55 @@ public class Client {
                 Scanner scanner = new Scanner(System.in);
                 while(socket.isConnected()){
                     String message = scanner.nextLine();
-                    bufferedWriter.write(username + ':' + message);
+                    bufferedWriter.write(clientUsername + ':' + message);
                     bufferedWriter.newLine();
                     bufferedWriter.flush();
                 }
             }catch (Exception e){
-                closeEverything(socket, bufferedWriter, bufferedReader);
+                closeEverything(socket, bufferedReader, bufferedWriter);
             }
         }
+    public void listenForMessages(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String message;
+                while (socket.isConnected()){
+                    try{
+                        message = bufferedReader.readLine();
+                        System.out.println(message);
+                    } catch (IOException e) {
+                        closeEverything(socket, bufferedReader, bufferedWriter);
+                    }
+                }
+            }
+        }).start();
+    }
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        try{
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if(socket != null){
+                socket.close();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your username: ");
+            String username = scanner.nextLine();
+            Socket socket = new Socket("localhost", 1234);
+            Client client = new Client(socket, username);
+            client.listenForMessages();
+            client.sendMessage();
+            
         try {
             Socket sock = new Socket("127.0.0.1", 6013);
             OutputStream out = sock.getOutputStream();
@@ -58,8 +97,8 @@ public class Client {
             System.out.println("Server response: \n" + result);
 
             sock.close();
-        } catch (IOException ioe) {
-            System.err.println(ioe);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
